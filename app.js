0be542799fb3621ebfd2a703f0dfff41f218c7c3
Don Jasa <LyndonJ@magenic.com>
2020-01-01 15:27:34 +0800

    enabled cors anywhere

--- app.js
+++ app.js
@@ -2,13 +2,16 @@ const express = require('express');
 require('./mongoose.js');
 
 const routes = require('./routes');
+var cors = require('cors');
 
 var app = express();
+
+app.use(cors());
+app.use(express.json());
+
 app.get('/', (req, res) => {
 	res.send('Hello Express')
 });
-
-app.use(express.json());
 app.use(routes);
 
 const port = process.env.PORT || 3000;
